850b9a43ad5f8caa2897c09ba1a5bb76bf0aa3ea
yuuki ite <decorated.knight99@gmail.com>
2019-04-14 07:05:08 +0900

    webpack.config.jsyしゅうせい

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,10 @@
-const path = require('path');
-
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
-  },
   target: 'node',
   module: {
     rules: [
       {
-        test: /\.m?js$/,
-        exclude: /(node_modules|bower_components)/,
+        test: /\.js$/,
+        exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
           options: {
